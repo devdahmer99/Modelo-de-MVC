70ea48713a37998be6d7c6fb3a6c086800dfd1c1
Dahmer <dahmer@pop-os.localdomain>
2022-06-07 00:34:48 -0300

    Finalização do modelo de mvc

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-session_start();
-require __DIR__ . 'config.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5ec62a18cb876f862ce3d5bf61cd16cb29d41d43',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5ec62a18cb876f862ce3d5bf61cd16cb29d41d43',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'clancats/hydrahon' => array(
+            'pretty_version' => 'v1.1.12',
+            'version' => '1.1.12.0',
+            'reference' => 'cc2fc3fd2ea082a2d531a751da49cc6a413270cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../clancats/hydrahon',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
